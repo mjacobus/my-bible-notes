85de2e83dba225bbb5149a156e01b804cb54165f
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-08-22 13:50:59 -0300

    Fix migration

--- db/migrate/20210822112304_create_timelines.rb
+++ db/migrate/20210822112304_create_timelines.rb
@@ -4,12 +4,14 @@ class CreateTimelines < ActiveRecord::Migration[6.1]
   def change
     create_table :timelines do |t|
       t.string :name
-      t.string :slug, index: { unique: true }, null: false
+      t.string :slug, null: false
       t.text :description
       t.boolean :public, default: false
       t.references :user
 
       t.timestamps
     end
+
+    add_index :timelines, [:slug, :user_id], unique: true
   end
 end
