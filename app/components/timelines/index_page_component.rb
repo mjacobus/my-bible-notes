396e398c059e990ffbd4ba5e37fbc5ef8de40464
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-08-22 10:57:16 -0300

    Add index page

--- app/components/timelines/index_page_component.rb
+++ app/components/timelines/index_page_component.rb
@@ -3,4 +3,14 @@
 class Timelines::IndexPageComponent < PageComponent
   has :timelines
   paginate :timelines
+
+  def new_link
+    link_to('foo')
+  end
+
+  def menu
+    DropdownMenuComponent.new(type: :list_options).tap do |menu|
+      menu.item { menu.link(t('app.links.new'), urls.new_timeline_path(current_user) ) }
+    end
+  end
 end
