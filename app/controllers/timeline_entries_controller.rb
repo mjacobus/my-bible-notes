23b1476793b4b7bd266352ba6dbeba7eceb39481
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-08-22 17:19:25 -0300

    WIP

--- app/controllers/timeline_entries_controller.rb
+++ app/controllers/timeline_entries_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class TimelineEntriesController < ApplicationController
+  include CrudController
+
+  skip_before_action :require_authorization
+
+  key :entry
+  permit :title, :year, :date_complement, :explanation, :precision, :confirmed
+  scope { current_user.timelines.by_slug(params[:timeline_id]) }
+  component_class_template 'TimelineEntries::%{type}PageComponent'
+
+  # def record
+  #   @record ||= find_scope.by_slug(params[:id])
+  # end
 end
--- spec/requests/timeline_entries_request_spec.rb
+++ spec/requests/timeline_entries_request_spec.rb
@@ -3,4 +3,215 @@
 require 'rails_helper'
 
 RSpec.describe TimelineEntriesController, type: :request do
+  # general
+  let(:timeline) { factory.create(user_id: current_user.id) }
+  let(:record) { factory.create(timeline_id: timeline.id) }
+  let(:factory) { factories.timeline_entries }
+  let(:scope) { timeline.entries.all }
+  let(:key) { model_class.to_s.underscore.split('/').last.to_sym }
+  let(:model_class) { Db::TimelineEntry }
+
+  # components
+  let(:index_component) { TimelineEntries::IndexPageComponent }
+  let(:show_component) { TimelineEntries::ShowPageComponent }
+  let(:form_component) { TimelineEntries::FormPageComponent }
+
+  # paths
+  let(:index_path) { routes.timeline_entries_path(timeline) }
+  let(:new_path) { routes.new_timeline_entry_path(timeline) }
+  let(:edit_path) { routes.edit_timeline_entry_path(record) }
+  let(:show_path) { routes.to(record) }
+
+  # attributes
+  let(:valid_attributes) { factory.attributes.merge(title: 'new title') }
+  let(:invalid_attributes) { factory.attributes.merge(title: '') }
+
+  describe 'GET #index' do
+    before { record }
+
+    let(:perform_request) { get(index_path) }
+
+    it 'responds with success' do
+      perform_request
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include(record.title)
+    end
+
+    it 'renders the correct component' do
+      mock_renderer
+
+      perform_request
+
+      expected_component = index_component.new(
+        key.to_s.pluralize.to_sym => scope,
+        current_user: current_user
+      )
+      expect(renderer).to have_rendered_component(expected_component)
+    end
+  end
+
+  describe 'GET #show' do
+    let(:perform_request) { get(show_path) }
+
+    it 'returns with success' do
+      perform_request
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the correct component' do
+      mock_renderer
+
+      perform_request
+
+      expected_component = show_component.new(
+        key => record,
+        current_user: current_user
+      )
+      expect(renderer).to have_rendered_component(expected_component)
+    end
+  end
+
+  describe 'GET #new' do
+    let(:perform_request) { get(new_path) }
+    let(:record) { model_class.new }
+
+    it 'returns with success' do
+      perform_request
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the correct component' do
+      mock_renderer
+
+      perform_request
+
+      expected_component = form_component.new(
+        key => scope.new,
+        current_user: current_user
+      )
+      expect(renderer).to have_rendered_component(expected_component)
+    end
+  end
+
+  describe 'POST #create' do
+    let(:perform_request) { post(index_path, params: params) }
+
+    context 'when payload is valid' do
+      let(:params) { { key => valid_attributes } }
+
+      it 'returns with success' do
+        perform_request
+
+        expect(response).to redirect_to(index_path)
+      end
+
+      it 'creates record' do
+        expect { perform_request }.to change(model_class, :count).by(1)
+      end
+    end
+
+    context 'when payload is invalid' do
+      let(:params) { { key => invalid_attributes } }
+
+      it 'responds with 422' do
+        perform_request
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 're-renders form' do
+        mock_renderer
+
+        perform_request
+
+        expected_component = form_component.new(
+          key => model_class.new(invalid_attributes),
+          current_user: current_user
+        )
+        expect(renderer).to have_rendered_component(expected_component)
+      end
+    end
+  end
+
+  describe 'GET #edit' do
+    let(:perform_request) { get(edit_path) }
+
+    it 'returns with success' do
+      perform_request
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the correct component' do
+      mock_renderer
+
+      perform_request
+
+      expected_component = form_component.new(
+        key => record,
+        current_user: current_user
+      )
+      expect(renderer).to have_rendered_component(expected_component)
+    end
+  end
+
+  describe 'PATCH #update' do
+    let(:perform_request) { patch(show_path, params: params) }
+
+    context 'when payload is valid' do
+      let(:params) { { key => valid_attributes } }
+
+      it 'redirects to index' do
+        perform_request
+
+        expect(response).to redirect_to(index_path)
+      end
+
+      it 'creates record' do
+        expect { perform_request }.to change { record.reload.title }.to('new title')
+      end
+    end
+
+    context 'when payload is invalid' do
+      let(:params) { { key => invalid_attributes } }
+
+      it 'returns with success' do
+        perform_request
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 're-renders form' do
+        mock_renderer
+
+        perform_request
+
+        record.attributes = invalid_attributes
+        expected_component = form_component.new(
+          key => record,
+          current_user: current_user
+        )
+        expect(renderer).to have_rendered_component(expected_component)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let(:perform_request) { delete(show_path) }
+
+    it 'redirects to index' do
+      perform_request
+
+      expect(response).to redirect_to(index_path)
+    end
+
+    it 'deletes record' do
+      record
+
+      expect { perform_request }.to change(model_class, :count).by(-1)
+    end
+  end
 end
