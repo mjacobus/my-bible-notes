b5cf95b8059dcadbe1aa180061daceb556e304cb
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-08-24 23:07:38 -0300

    Refactor lane

--- app/models/timeline/lane.rb
+++ app/models/timeline/lane.rb
@@ -17,20 +17,20 @@ module Timeline
     end
 
     def accept?(candidate)
-      events.each do |event|
-        if candidate.overlap_with?(event, inclusive: false)
-          return false
-        end
-
-        if reject_by_single_year?(candidate, event)
-          return false
-        end
-      end
-
-      true
+      !reject?(candidate)
     end
 
     private
+
+    def reject?(candidate)
+      events.any? do |event|
+        if candidate.overlap_with?(event, inclusive: false)
+          return true
+        end
+
+        reject_by_single_year?(candidate, event)
+      end
+    end
 
     def reject_by_single_year?(candidate, event)
       if candidate.single_year? && (candidate.time.to.to_i == event.time.from.to_i)
