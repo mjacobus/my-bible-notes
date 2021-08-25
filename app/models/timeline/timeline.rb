628c14ccc499ca7b59eb1af48ccdffa254c86a21
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-08-24 23:30:26 -0300

    Create timeline

---
+++ app/models/timeline/timeline.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Timeline
+  class Timeline
+    attr_reader :name
+    attr_reader :lanes
+
+    def initialize(attributes = {})
+      @name = attributes.fetch(:name)
+      @lanes = []
+    end
+
+    def add_event(event)
+      lanes.each do |lane|
+        if lane.add_event(event)
+          # rubocop:disable Lint/NonLocalExitFromIterator
+          return
+          # rubocop:enable Lint/NonLocalExitFromIterator
+        end
+      end
+
+      create_lane(event)
+    end
+
+    private
+
+    def create_lane(event)
+      lane = Lane.new(lanes.length)
+      lane.add_event(event)
+      @lanes.push(lane)
+    end
+  end
+end
