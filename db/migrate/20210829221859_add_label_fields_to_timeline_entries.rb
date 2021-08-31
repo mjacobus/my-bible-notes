5949c68d3c46031568ecdf9dc83e2958c99440b1
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-08-31 19:33:02 -0300

    Appease rubocop

--- db/migrate/20210829221859_add_label_fields_to_timeline_entries.rb
+++ db/migrate/20210829221859_add_label_fields_to_timeline_entries.rb
@@ -6,7 +6,7 @@ class AddLabelFieldsToTimelineEntries < ActiveRecord::Migration[6.1]
     add_column :timeline_entries, :text_properties, :json
 
     reversible do
-      execute("UPDATE timeline_entries set text = title")
+      execute('UPDATE timeline_entries set text = title')
     end
   end
 end
