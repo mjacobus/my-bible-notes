6e4f0ae348995ea30f22d6530bf496a68937df1a
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-09-01 19:45:49 -0300

    Refactor timeline breadcrumb

--- app/components/timelines/index_page_component.rb
+++ app/components/timelines/index_page_component.rb
@@ -1,26 +1,27 @@
 # frozen_string_literal: true
 
-class Timelines::IndexPageComponent < PageComponent
-  has :timelines
-  has :owner
-  paginate :timelines
+module Timelines
+  class IndexPageComponent < PageComponent
+    has :timelines
+    has :owner
+    paginate :timelines
 
-  private
+    private
 
-  def new_link
-    link_to('foo')
-  end
-
-  def setup
-    with_owner_breadcrumb
-    breadcrumb.add(t('app.links.timelines'))
-  end
-
-  def menu_items(menu)
-    unless visitor.is?(owner)
-      return []
+    def new_link
+      link_to('foo')
     end
 
-    [menu.link(t('app.links.new'), urls.new_timeline_path(current_user))]
+    def breadcrumb
+      @breadcrumb ||= BreadcrumbComponent.new.under_profile(owner).index
+    end
+
+    def menu_items(menu)
+      unless visitor.is?(owner)
+        return []
+      end
+
+      [menu.link(t('app.links.new'), urls.new_timeline_path(current_user))]
+    end
   end
 end
--- app/components/timelines/show_page_component.rb
+++ app/components/timelines/show_page_component.rb
@@ -1,31 +1,31 @@
 # frozen_string_literal: true
 
-class Timelines::ShowPageComponent < PageComponent
-  include MenuAwareComponent
-  menu_type :list_options
-  has :timeline
-  has :current_user, optional: true
-  has :owner
+module Timelines
+  class ShowPageComponent < PageComponent
+    include MenuAwareComponent
+    menu_type :list_options
+    has :timeline
+    has :current_user, optional: true
+    has :owner
 
-  private
+    private
 
-  def setup
-    with_owner_breadcrumb
-    breadcrumb.add(t('app.links.timelines'), urls.timelines_path(owner))
-    breadcrumb.add(timeline.name)
-  end
-
-  def menu_items(menu)
-    if visitor.is?(current_user)
-      [
-        menu.link(t('app.links.timeline_entries'), urls.timeline_entries_path(timeline)),
-        menu.link(t('app.links.edit'), urls.edit_timeline_path(timeline)),
-        menu.link(t('app.links.delete'), urls.to(timeline), data: { method: :delete, confirm: delete_warning })
-      ]
+    def breadcrumb
+      @breadcrumb ||= BreadcrumbComponent.new.under_profile(owner).show(timeline)
     end
-  end
 
-  def delete_warning
-    t('app.messages.confirm_delete')
+    def menu_items(menu)
+      if visitor.is?(current_user)
+        [
+          menu.link(t('app.links.timeline_entries'), urls.timeline_entries_path(timeline)),
+          menu.link(t('app.links.edit'), urls.edit_timeline_path(timeline)),
+          menu.link(t('app.links.delete'), urls.to(timeline), data: { method: :delete, confirm: delete_warning })
+        ]
+      end
+    end
+
+    def delete_warning
+      t('app.messages.confirm_delete')
+    end
   end
 end
