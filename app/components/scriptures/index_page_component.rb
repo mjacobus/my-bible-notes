6d961529d5c55cfae77a6a2123349b788cecb5ee
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-09-01 16:07:36 -0300

    UP

--- app/components/scriptures/index_page_component.rb
+++ app/components/scriptures/index_page_component.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
-class Scriptures::IndexPageComponent < ApplicationComponent
-  has :scripture
+class Scriptures::IndexPageComponent < PageComponent
+  include MenuAwareComponent
+  has :scriptures
+  paginate :scriptures
   has :owner
+  menu_type :item_options
+
+  private
+
+  def menu_items(menu)
+    []
+  end
+
+  def delete_warning
+    t('app.messages.confirm_delete')
+  end
 end
