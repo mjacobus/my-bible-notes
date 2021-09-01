b500f070e3fa5c157634188d7d828cb430cef861
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-09-01 19:48:05 -0300

    Cleanup

--- app/components/timelines/form_page_component.rb
+++ app/components/timelines/form_page_component.rb
@@ -14,17 +14,5 @@ module Timelines
     def setup
       # TODO: Remove later
     end
-    # def setup
-    #   with_owner_breadcrumb
-    #   breadcrumb.add(t('app.links.timelines'), urls.timelines_path(current_user))
-    #
-    #   if timeline.id?
-    #     breadcrumb.add(timeline.name, urls.to(timeline))
-    #     breadcrumb.add(t('app.links.edit'))
-    #     return
-    #   end
-    #
-    #   breadcrumb.add(t('app.links.new'))
-    # end
   end
 end
