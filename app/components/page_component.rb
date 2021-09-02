cb6779669c39e511cf7d999c7e2545a56e0a8fb4
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-09-01 21:31:17 -0300

    Removed unused methods

--- app/components/page_component.rb
+++ app/components/page_component.rb
@@ -4,16 +4,6 @@ class PageComponent < ApplicationComponent
   include MenuAwareComponent
   menu_type :list_options
 
-  def with_owner_breadcrumb
-    breadcrumb.add(owner.username)
-  end
-
-  def breadcrumb
-    @breadcrumb ||= BreadcrumbComponent.new.tap do |b|
-      b.add(t('app.links.home'), urls.root_path)
-    end
-  end
-
   def flash
     FlashComponent.new
   end
