95178db086cffc61fd91d2928130ceec669f7644
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-09-01 21:31:17 -0300

    Removed unused methods

--- app/components/profile/edit_page_component.rb
+++ app/components/profile/edit_page_component.rb
@@ -4,10 +4,11 @@ module Profile
   class EditPageComponent < BaseFormPageComponent
     has :user
 
-    def initialize(*args)
-      super
-
-      breadcrumb.add(t('app.links.profile'))
+    def breadcrumb
+      @breadcrumb ||= BreadcrumbComponent.new.tap do |b|
+        b.add(t('app.links.home'), urls.root_path)
+        b.add(t('app.links.profile'))
+      end
     end
 
     def url
