aac3f83c2bc5e89e711446b4fcec0435f3e60668
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-09-05 14:05:21 -0300

    Display basic search form for scriptures

--- app/components/scriptures/search_form_component.rb
+++ app/components/scriptures/search_form_component.rb
@@ -15,5 +15,17 @@ module Scriptures
     def has_filters?
       search_params.any?(:tags, :title)
     end
+
+    def selected_tags
+      @selected_tags ||= profile_owner.tags.scripture.where(slug: tag_slugs)
+    end
+
+    def tag_slugs
+      search_params[:tags].to_s.split(',').map(&:strip)
+    end
+
+    def selected_tags_message
+      t('app.messages.selected_tags', tag_names: selected_tags.map(&:name).join(', '))
+    end
   end
 end
--- app/models/db/tag.rb
+++ app/models/db/tag.rb
@@ -4,6 +4,8 @@ class Db::Tag < ApplicationRecord
   has_and_belongs_to_many :scriptures
   belongs_to :user
 
+  scope :scripture, -> { where(type: 'Db::ScriptureTag') }
+
   validates :name, presence: true, uniqueness: { case_sensitive: false, scope: [:type] }
 
   def name=(value)
