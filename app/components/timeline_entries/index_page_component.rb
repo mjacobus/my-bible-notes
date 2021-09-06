3ff8b201730fbb6cb74453106bd57a6fcc5d6b18
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-09-06 14:57:18 -0300

    Remove unused code

--- app/components/timeline_entries/index_page_component.rb
+++ app/components/timeline_entries/index_page_component.rb
@@ -18,9 +18,5 @@ module TimelineEntries
     def breadcrumb
       @breadcrumb ||= BreadcrumbComponent.new.under_profile(profile_owner).index(timeline)
     end
-
-    def menu_items(menu)
-      [menu.link(t('app.links.new'), urls.new_timeline_entry_path(timeline))]
-    end
   end
 end
--- app/components/timelines/show_page_component.rb
+++ app/components/timelines/show_page_component.rb
@@ -18,19 +18,5 @@ module Timelines
     def breadcrumb
       @breadcrumb ||= BreadcrumbComponent.new.under_profile(profile_owner).show(record)
     end
-
-    def menu_items(menu)
-      if visitor.is?(current_user)
-        [
-          menu.link(t('app.links.timeline_entries'), urls.timeline_entries_path(record)),
-          menu.link(t('app.links.edit'), urls.edit_timeline_path(record)),
-          menu.link(t('app.links.delete'), urls.to(record), data: { method: :delete, confirm: delete_warning })
-        ]
-      end
-    end
-
-    def delete_warning
-      t('app.messages.confirm_delete')
-    end
   end
 end
