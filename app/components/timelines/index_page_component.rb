e904b6e8467cf16f94f3f14e1dc8406b15304076
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-09-06 14:57:18 -0300

    Remove unused code

--- app/components/timelines/index_page_component.rb
+++ app/components/timelines/index_page_component.rb
@@ -13,20 +13,8 @@ module Timelines
 
     private
 
-    def new_link
-      link_to('foo')
-    end
-
     def breadcrumb
       @breadcrumb ||= BreadcrumbComponent.new.under_profile(profile_owner).index
-    end
-
-    def menu_items(menu)
-      unless visitor.is?(profile_owner)
-        return []
-      end
-
-      [menu.link(t('app.links.new'), urls.new_timeline_path(current_user))]
     end
   end
 end
